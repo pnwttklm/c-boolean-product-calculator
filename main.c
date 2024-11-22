39089a1136e263c3cb25ac24d350bd773f1f1ac6
Poonyawatt  Klumnaim <poon@pnwttklm.com>
2024-11-23 00:26:40 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main(void) {
+  int rowmat1, columnmat1, rowmat2, columnmat2, collector, sum = 0;
+  printf("\nPlease Enter Sizes of Matrix 1.\n");
+  printf("Row = ");
+  scanf("%d", &rowmat1);
+  printf("Column = ");
+  scanf(" %d", &columnmat1);
+  printf("\nPlease Enter Sizes of Matrix 2.\n");
+  do {
+    printf("Row = ");
+    scanf("%d", &rowmat2);
+    printf("Column = ");
+    scanf(" %d", &columnmat2);
+    if (columnmat1 != rowmat2) {
+      printf("ERROR: Doing Matrix Multiplication The Matrix1's Column Must "
+             "Equal to The Matrix2's Row.\nPlease Enter Please Enter Sizes of "
+             "Matrix 2 Again.\n");
+    }
+  } while (columnmat1 != rowmat2);
+  int mat1[rowmat1][columnmat1];
+  int mat2[rowmat2][columnmat2];
+  int matrix[rowmat1][columnmat2];
+  printf("\nPlease Enter Values of Matrix 1.\n");
+  for (int row = 0; row < rowmat1; row++) {
+    for (int column = 0; column < columnmat1; column++) {
+      scanf(" %d", &mat1[row][column]);
+    }
+  }
+  printf("\nPlease Enter Values of Matrix 2.\n");
+  for (int row = 0; row < rowmat2; row++) {
+    for (int column = 0; column < columnmat2; column++) {
+      scanf(" %d", &mat2[row][column]);
+    }
+  }
+  printf("\nRESULT.\n");
+  for (int row = 0; row < rowmat1; row++) {
+    for (int column = 0; column < columnmat2; column++) {
+      matrix[row][column] = 0;
+      for (int time = 0; time < columnmat1; time++) {
+        collector = 0;
+        if (mat1[row][time] == 1 && mat2[time][column] == 1) {
+          collector++;
+        }
+        if (collector > 0) {
+          matrix[row][column] = 1;
+        }
+      }
+      sum = sum + matrix[row][column];
+      printf("%d ", matrix[row][column]);
+    }
+    printf("\n");
+  }
+  printf("Σ = %d", sum);
+  return 0;
+}
